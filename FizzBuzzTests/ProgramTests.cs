6fe2b075beea6da797c03f0bb78c4e69fe302f4f
Jamie Jordan <jj_sw@outlook.com>
2019-01-09 23:02:35 +0000

    initial commit

---
+++ FizzBuzzTests/ProgramTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xunit;
+
+namespace FizzBuzzTests
+{
+    public class ProgramTests
+    {
+        [Fact]
+        public void TestName()
+        {
+        //Given
+        
+        //When
+        
+        //Then
+        Assert.Equal(1,1);
+        }
+    }
+}
