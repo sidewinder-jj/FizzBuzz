faff629e39325b09d898d02d55a5e05abada2cbc
Jamie Jordan <jj_sw@outlook.com>
2019-01-10 01:22:22 +0000

    FizzBuzzConverter returns buzz for multiples of 5

--- FizzBuzz/FizzBuzzConverter.cs
+++ FizzBuzz/FizzBuzzConverter.cs
@@ -8,6 +8,9 @@ namespace FizzBuzzKata
         {
             if (input % 3 == 0) 
                 return "Fizz";
+
+            if (input % 5 == 0)
+                return "Buzz";
             
             return input.ToString();
         }
