0dd7aaab69eeb2d7e13400af6d3599af0337c213
Jamie Jordan <jj_sw@outlook.com>
2019-01-10 00:44:50 +0000

    return fizz for multiple of three

---
+++ FizzBuzz/FizzBuzzConverter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FizzBuzzKata
+{
+    public class FizzBuzzConverter
+    {
+        public string Convert(int input)
+        {
+            return "Fizz";
+        }
+    }
+}
---
+++ FizzBuzzTests/FizzBuzzConverterTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using FizzBuzzKata;
+using Xunit;
+
+namespace FizzBuzzTests
+{
+    public class FizzBuzzConverterTests
+    {
+        const string fizz = "Fizz";
+        
+        [Fact]
+        public void FizzBuzzConverter_ReturnsFizz_MultipleOfThree()
+        {
+            var multipleOfThree = 3;
+            var fizzBuzz = new FizzBuzzConverter();
+
+            var result = fizzBuzz.Convert(multipleOfThree);
+            
+            Assert.Equal(fizz, result);
+        }
+
+        [Fact]
+        public void FizzBuzzConverter_ReturnsInputNumber_NotMultipleOfThreeOrFive()
+        {
+            var numberWhichIsntMultiple = 2;
+            var fizzBuzz = new FizzBuzzConverter();
+
+            var result = fizzBuzz.Convert(numberWhichIsntMultiple);
+
+            Assert.Equal(numberWhichIsntMultiple.ToString(), result);
+        }
+    }
+}
